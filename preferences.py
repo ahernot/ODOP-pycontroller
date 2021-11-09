8fe75dc1505290516f889cc5edcbf945dbcae02e
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 21:54:36 +0100

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -3,3 +3,10 @@ BAUD_RATE = 9600
 READY_MSG = b'Controller ready\r\n'
 TIME_INIT_MAX = 60  # in seconds
 
+# X-axis angular range
+X_ANGLE_MIN = -20.
+X_ANGLE_MAX = 90.
+
+SESSION_PATH = "c:\\tmp\\"
+FILENAME_TEMPLATE = "odop-vI{}-vA{}-hI{}-hA{}.jpg"
+ENABLE_DGCC = True
