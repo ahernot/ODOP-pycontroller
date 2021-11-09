4a623817de158b226557f11ea14496e15deb5875
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 22:07:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+# Copyright (C) 2021 Anatole Hernot (github.com/ahernot), Mines Paris (PSL Research University). All rights reserved.
+
 import time
 
 from controller import ODOP
 from composer import Composer
-#from preferences import *
+
+
 
 if __name__ == '__main__':
     
+    # Initialise ODOP
     odop = ODOP()
 
     while not odop.ready:
         time.sleep(0.1)
 
-    composer = Composer (odop, 10, 36)
+    # Initialise composer
+    composer = Composer(
+        odop=odop,
+        vertical_shots_nb=10,
+        horizontal_shots_nb=36
+    )
+
+    # Run composer
+    composer.run()
