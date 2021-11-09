123d743d740a8a3cd4d9398e36aa679114389bd8
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 21:29:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import time
 
 from controller import ODOP
+from composer import *
 #from preferences import *
 
 if __name__ == '__main__':
-    
-    odop = ODOP()
-
-    while not odop.ready:
-        time.sleep(0.1)
-    
-    success = odop.execute ('status', b'Status ok\r\n')
-    print(success)
-
-    success = odop.execute ('angle x 10', b'angle_x: success\r\n', 20.)
-    print(success)
-
-    odop.read()
+    pass
