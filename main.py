b928c3732e457e367bfaf7e08ef6762c141d34e7
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 21:54:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import time
 
 from controller import ODOP
-from composer import *
+from composer import Composer
 #from preferences import *
 
 if __name__ == '__main__':
-    pass
+    
+    odop = ODOP()
+
+    while not odop.ready:
+        time.sleep(0.1)
+
+    composer = Composer (odop, 10, 36)
