6c56362e0956b0059925646982174d2dfef51785
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 17:34:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import time
 
+from controller import ODOP
+#from preferences import *
 
 if __name__ == '__main__':
-    pass
+    
+    odop = ODOP()
+
+    while not odop.ready:
+        time.sleep(0.1)
+
+    print(odop.ready)
+
+    x = odop.execute ('status')
+
+    odop.read()
