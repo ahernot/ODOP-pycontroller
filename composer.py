cc4c6788643eea305322bf71ab24569272599ad7
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-09 21:54:26 +0100

    create Composer

--- composer.py
+++ composer.py
@@ -1,12 +1,10 @@
 import time
 
 from controller import ODOP
+from preferences import *
 
-odop = ODOP()
 
-while not odop.ready:
-    time.sleep(0.1)
-
+"""
 success = odop.execute ('status', b'Status ok\r\n')
 print(success)
 
@@ -14,31 +12,43 @@ success = odop.execute ('angle x 10', b'angle_x: success\r\n', 20.)
 print(success)
 
 odop.read()
+"""
 
 
 
 
-for vShot in range(NUMBER_OF_VERTICAL_SHOTS):
-    print("\tCurrent vAngle is {}".format(currentVerticalAngle));
+class Composer:
 
-    print("\tReseting hAngle. Was {}".format(currentHorizontalAngle));
-    currentHorizontalAngle=0
-    
-    odopGoTo(currentVerticalAngle)
-    for hShot in range(NUMBER_OF_HORIZONTAL_SHOTS):
-        print("\t\tCurrent hAngle is {}".format(currentHorizontalAngle));
-        
-        #Build file name
-        fileLocationAndName=SESSION_PATH + FILENAME_TEMPLATE.format(vShot,currentVerticalAngle,hShot,currentHorizontalAngle)
-        print("\t\tRequesting shot:"+fileLocationAndName)
-        #Take shot
-        takePhoto(fileLocationAndName)
+    def __init__ (self, odop: ODOP, vertical_shots_nb: int, horizontal_shots_nb: int):
+        self.odop = odop
+        self.vertical_shots_nb = vertical_shots_nb
+        self.horizontal_shots_nb = horizontal_shots_nb
 
-        #Rotate the H-table
-        horizontalRelativeStep()
-        currentHorizontalAngle=currentHorizontalAngle+angleBetweenHorizontalShots
+        x_step_deg = (X_ANGLE_MAX - X_ANGLE_MIN) / (vertical_shots_nb - 1)
+        y_step_deg = 360 / horizontal_shots_nb
 
-    print("Done horizontal cycle")
-    currentVerticalAngle=currentVerticalAngle+angleBetweenVerticalShots
-    
-print("Done vertical cycle")
+    def run (self):
+
+        for vertical_shot_id in range (self.vertical_shots_nb + 1):
+            #print("\tCurrent vAngle is {}".format(currentVerticalAngle))
+
+            # Reset Y-axis angle
+            #print("\tReseting hAngle. Was {}".format(currentHorizontalAngle))
+            #currentHorizontalAngle = 0
+            self.odop .move_absolute ('y', 0)
+
+            for horizontal_shot_id in range (self.horizontal_shots_nb):
+                #print("\t\tCurrent hAngle is {}".format(currentHorizontalAngle))
+
+                # Build file name
+                file_path = SESSION_PATH + FILENAME_TEMPLATE.format(vertical_shot_id, currentVerticalAngle, horizontal_shot_id, currentHorizontalAngle)
+                print("\t\tRequesting shot: " + fileLocationAndName)
+
+                # Take shot
+                #takePhoto(fileLocationAndName)
+
+                # Move on Y-axis
+                self.odop .move_relative ('y', self.y_step_deg)
+
+            # Move on X-axis
+            self.odop .move_relative ('x', self.x_step_deg)
