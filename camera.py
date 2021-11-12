cd14cc58b4d5426a1e513310a39a17fdf727eadf
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-12 08:31:21 +0100

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+
+from preferences import *
+
+
+
+def process_exists(process_name):
+    #output = ""
+    output = subprocess.Popen('tasklist /FI "imagename eq {process_name}"' , stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE) .communicate()[0]
+    
+    # Check in last line for process name
+    last_line = bytes(output)
+    last_line = output.split(bytes('\r\n', 'utf-8'))
+    try:
+        last_line = last_line[3].lower()
+    except:
+        return False
+        
+    print(f'last line: {last_line}')
+
+    # Because Fail message could be translated
+    pname = bytes(process_name, 'utf-8')
+    if (pname.lower() in last_line):
+        return True
+    else:
+        return False
+
+
+class Camera:
+
+    def __init__ (self):
+        self.__ready = False
+        self.update_status()
+
+    def update_status (self):
+        if (process_exists(dgccMainEXE)):
+            self.__ready = True
+        else:
+            print('DGCC is not running. Please launch the app and then run Camera.update_status()')
+            self.__ready = False
+
+    def ready (self):
+        return self.__ready
+
+    def capture (self, filepath: str):
+        command = f'"{dgccTool}" {dgccSnapCommand} {filepath}'
+        os.system(command)
+        return True
