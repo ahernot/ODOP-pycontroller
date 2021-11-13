308d5a205f7fb215716a8abfdccfcea10fb9359d
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-13 14:34:19 +0100

    command execution overhaul

--- controller.py
+++ controller.py
@@ -5,9 +5,10 @@ import time
 
 from preferences import *
 
-
+#TODO: tunable parameters for estimate_zero -200, +25
 #TODO: python needs to print whatever the controller sends through serial
 #TODO: tune time_window for move commands, AND FOR ABSOLUTE COMMANDS IN PARTICULAR
+#TODO: get_version and get_status
 
 
 class Controller:
@@ -36,16 +37,22 @@ class Controller:
     def ready (self):
         return self.__ready
 
-    def execute (self, command: str, time_window = 2., readback = b'', fatal = b''):
-        print(f'\nExecuting {command}')
+    def execute (self, command: str, time_window = 2., readback = b'', fatal = b'') -> int:
+        """
+        0 for readback==readback
+        1 for no readback even though readback specified
+        2 for fatal
+        """
 
         # Send command
+        print(f'\nExecuting {command}')
         self.controller .write(bytes(command, 'utf-8'))
         time.sleep(0.05)
 
         # Process readback
         time_start = time.time()  # in seconds
         # print(f'{int(time_start)} - waiting till {int(time_start + time_window)} ({time_window} seconds)')
+        
         while time.time() < time_start + time_window:
             # Read data
             data = self.controller .readline()
@@ -53,11 +60,12 @@ class Controller:
             if data:
                 print(data) #.decode('utf-8')[:-1])  ##################PRINT
                 if readback and data == readback:  # Exit loop with success
-                    return True
+                    return 0
                 if fatal and data == fatal:  # Exit loop with failure
-                    return False
-        
-        return not (not readback)
+                    return 2
+
+        if readback: return 1
+        else: return 0  # True if readback unspecified
 
 
     def read (self, time_window: float = 2.):
@@ -74,57 +82,86 @@ class ODOP (Controller):
 
     def __init__ (self, baud_rate: int = BAUD_RATE, port = PORT, ready_msg = READY_MSG, time_init_max = TIME_INIT_MAX):
         super(ODOP, self).__init__ (baud_rate, port, ready_msg, time_init_max)
-        self.__angles = {'x': 0, 'y': 0}
+        self.__angles = {'x': 0., 'y': 0.}
 
-    # def get_version (self): self.execute ('version')  # issue: doesn't print version
+    # def get_version (self): self.execute ('version')
+
     def get_status (self):
-        self.execute (command='status', time_window=2., readback=b'Status ok\r\n')
+        val = self.execute (command='status', time_window=2., readback=b'Status ok\r\n')
+        if val == 0: return True, ''
+        else: return False, 'unable to verify status'
 
-    def get_angle (self, axis: str):
+    def get_angle (self, axis: str) -> float:
         if axis not in ('x', 'y'): return None
         return self.__angles [axis]
 
+
     # Calibration
-    def estimate_zero (self):
+    def estimate_zero (self) -> tuple:
         #self.execute ('estimate_zero', b'estimate_zero: success', time_window=120.)  # 2min
 
         # Move to bottom of range
-        success = self.execute (
+        val = self.execute (
             command='move_rel x -200',
-            time_window=120.,
+            time_window=2., #120., ############################# DEBUG
             readback=b'move_rel x: limit reached\r\n',
             fatal=b'move_rel x: success\r\n'
         )
-        if not success: return False, 'unable to reach minimum'  # Exit if didn't find end stop
-        #if success: return False, '' ########################### FOR TESTS ONLY
+        print(val)
+        #if val != 0: return False, 'unable to reach minimum'  # Exit if didn't find end stop ############################# DEBUG
 
         # Move up to estimate zero position
         success, msg = self.move_relative ('x', 25.)
-        return success
+        return success, msg
 
-    def set_zero (self):
-        return self.execute (command='set_zero', time_window=2., readback=b'set_zero: success\r\n')
+
+    def set_zero (self) -> bool:
+        val = self.execute (command='set_zero', time_window=2., readback=b'set_zero: success\r\n')
+        return val == 0
+
 
     # Motion
-    def move_relative (self, axis: str, value: float):
+    def move_relative (self, axis: str, value: float) -> tuple:
+
+        # Assert arguments
         axis = axis.lower()
         if axis not in ('x', 'y'): return False
+
+        # Log movement
         self.__angles [axis] += value
-        return self.execute (
+
+        # Send command
+        val = self.execute (
             command=f'move_rel {axis} {float(value)}',
             time_window=min(max(abs(2*value), 2), 60),  # no more than 60sec
             readback=bytes(f'move_rel {axis}: success\r\n', 'utf-8'),
             fatal=bytes(f'move_rel {axis}: limit reached\r\n', 'utf-8')
-        )  
+        )
 
-    def move_absolute (self, axis: str, value: float):
+        # Process output
+        if   val == 0: return True, ''
+        elif val == 2: return False, 'limit reached'
+        else:          return False, 'timeout'
+
+
+    def move_absolute (self, axis: str, value: float) -> tuple:
+
+        # Assert arguments
         axis = axis.lower()
         if axis not in ('x', 'y'): return False
+
+        # Log movement
         self.__angles [axis] = value
-        return self.execute (
+
+        # Send command
+        val = self.execute (
             command=f'move_abs {axis} {float(value)}',
             time_window=30.,
             readback=bytes(f'move_abs {axis}: success\r\n', 'utf-8'),
             fatal=bytes(f'move_abs {axis}: limit reached\r\n', 'utf-8')
         )
-    
+
+        # Process output
+        if   val == 0: return True, ''
+        elif val == 2: return False, 'limit reached'
+        else:          return False, 'timeout'
