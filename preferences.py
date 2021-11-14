fcdbfee91d7a5f26d9a2ffaab0f3b0c533f82468
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-14 22:26:20 +0100

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -19,7 +19,7 @@ X_ANGLE_MIN = -15.
 X_ANGLE_MAX = 90.
 
 # Delays
-TIME_INIT_MAX = 60  # in seconds
+TIME_INIT_MAX = 60.  # in seconds
 TIME_WINDOW_MIN = 2.  # Minimum time window to execute a command
 TIME_WINDOW_MAX = 120.  # Maximum time window to execute a command (default for move_abs commands)
 DELAY_BEFORE_SHOT = 1.
@@ -34,3 +34,5 @@ RUN_MESSAGE = 'Make sure to remove all calibration cables before starting run.'
 
 # Time windows must be wide enough to avoid having backlogged processes and the completion message of the previous process validating the next one
 # Document that Y channel is used for xLimMax
+
+# ODOP needs to be recalibrated every time a new ODOP() object is created, due to the fact that the locally-stored positional information is reset
