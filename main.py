d0ef4c66360cd1d46440bbb2b50b5998b308a311
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-14 22:26:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,44 +4,45 @@ import time
 
 from camera import Camera, CameraPlaceholder
 from controller import ODOP
-from composer import Composer
+from composition import Composition
+
+from functions import type_input
 from preferences import *
 
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
+    
     # Initialise camera
-    camera = CameraPlaceholder()  # Camera()
+    camera = CameraPlaceholder()  # Camera() ############################# DEBUG
     while not camera.ready():
         time.sleep(0.1)
         camera.update_status()
-    
 
     # Initialise ODOP
     odop = ODOP()
     while not odop.ready():
         time.sleep(0.1)
 
-
     # Calibrate ODOP
-    print('\nCalibrating ODOP')
-    success, msg = odop.estimate_zero()
-    if not success: raise InterruptedError(f'estimate_zero failure - {msg}')
-    success, msg = odop.adjust_position('x')
-    if not success: raise InterruptedError(f'manual calibration failure - {msg}')
-    odop.set_zero()
+    odop.calibrate()
 
 
-    # Initialise composer
-    composer = Composer(
-        odop=odop,
-        camera=camera,
-        vertical_shots_nb=10, #6,
-        horizontal_shots_nb=36 #4
-    )
+    while True:
 
-    input(f'\nComposer initialised. Press any key to begin run.\n{RUN_MESSAGE}')
+        vertical_shots_nb   = type_input('Enter the number of vertical shots\n> ', int)
+        horizontal_shots_nb = type_input('Enter the number of horizontal shots\n> ', int)
 
-    # Run composer
-    success, msg = composer.run()
-    if not success: raise InterruptedError(f'run failure - {msg}')
+        # Create composition
+        composition = Composition(
+            odop=odop,
+            camera=camera,
+            vertical_shots_nb=vertical_shots_nb,
+            horizontal_shots_nb=horizontal_shots_nb
+        )
+
+        input(f'\nComposer initialised. Press any key to begin run.\n{RUN_MESSAGE}')
+
+        # Run composer
+        success, msg = composition.run()
+        if not success: raise InterruptedError(f'run failure - {msg}')
